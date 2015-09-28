a340ff85d32e8d833838728b88f2c144b41ed9b0
pietroTP <pietro.grandi.3d@gmail.com>
2015-09-28 10:05:37 +0200

    physijs scene setup ok

--- js/collisions.js
+++ js/collisions.js
@@ -10,6 +10,9 @@ var container, renderer, scene, camera;
 function startScene() {
     'use strict';
 
+    Physijs.scripts.worker = '/js/lib/physis_worker.js';
+    Physijs.scripts.ammo = '/js/lib/ammo.js';
+
     // boilerplate
     container = document.getElementById('theContainer');
 
@@ -17,7 +20,7 @@ function startScene() {
     renderer.setClearColor(0xaabbff);
     renderer.setSize(640, 480);
 
-    scene = new THREE.Scene();
+    scene = new Physijs.Scene();
 
     camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 8000);
     camera.position.set(0, 0, 40);
@@ -33,6 +36,17 @@ function startScene() {
     var theLight2 = new THREE.DirectionalLight(0xddddff, 0.7);
     theLight2.position.set(10, 10, -20);
     scene.add(theLight2);
+
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var material = new THREE.MeshLambertMaterial(
+        {
+            color: 0xff4488
+        }
+    );
+
+    var mesh = new Physijs.BoxMesh( cubeGeometry, material );
+
+    scene.add(mesh);
 
     function animate()
     {
